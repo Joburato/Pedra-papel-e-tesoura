c44497aec3c1852ca9c82dc36e2041a3b121daca
Eduardo Job <eduardoJob@users.noreply.github.com>
2020-04-17 09:03:21 -0300

    Update tesoura.js

--- tesoura.js
+++ tesoura.js
@@ -1,8 +1,7 @@
-const OPTIONS = ["pedra", "papel", "tesoura"]
 result=0
 
 function play(){
-    var CHOOSE = [Math.floor(Math.random()*OPTIONS.length)]
+    var CHOOSE = [Math.floor(Math.random()*3)]
     if (CHOOSE==0){result="pedra"}
     if (CHOOSE==1){result="papel"}
     if (CHOOSE==2){result="tesoura"}
