d1fabde1c0dc6653941e439dab586b48aff3b7c9
Eduardo Job <eduardoJob@users.noreply.github.com>
2020-04-17 10:41:58 -0300

    Layout Changes

--- tesoura.js
+++ tesoura.js
@@ -5,5 +5,6 @@ function play(){
     if (CHOOSE==0){result="pedra"}
     if (CHOOSE==1){result="papel"}
     if (CHOOSE==2){result="tesoura"}
+    frase.innerHTML="O Computador escolheu..."
     resultado.innerHTML=result
 }
