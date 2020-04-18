7480df9bdf29f221260ef638419f33048ef14c16
Eduardo Job <eduardoJob@users.noreply.github.com>
2020-04-18 18:14:19 -0300

    update

--- tesoura.js
+++ tesoura.js
@@ -4,15 +4,21 @@ function play(){
     var CHOOSE = [Math.floor(Math.random()*3)]
     if (CHOOSE==0){
         result="pedra",
-        document.getElementById( "img1" ).style.display = "none"
+        document.getElementById( "img1" ).style.display = "none",
+        document.getElementById( "img2" ).style.display = "inline",
+        document.getElementById( "img3" ).style.display = "none"
     }
     if (CHOOSE==1){
         result="papel",
-        document.getElementById( "img1" ).style.display = "none";
+        document.getElementById( "img1" ).style.display = "none",
+        document.getElementById( "img2" ).style.display = "none",
+        document.getElementById( "img3" ).style.display = "inline"
     }
     if (CHOOSE==2){
         result="tesoura",
-        document.getElementById( "img1" ).style.display = "inline";
+        document.getElementById( "img1" ).style.display = "inline",
+        document.getElementById( "img2" ).style.display = "none",
+        document.getElementById( "img3" ).style.display = "none"
     }
     frase.innerHTML="<br><br><br>O Computador escolheu..."
     resultado.innerHTML=result
