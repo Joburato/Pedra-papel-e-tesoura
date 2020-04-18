073a1a974e6eaef7793afb2a317725fa7f3cf7b2
Eduardo Job <eduardoJob@users.noreply.github.com>
2020-04-18 13:38:07 -0300

    image codes

--- tesoura.js
+++ tesoura.js
@@ -2,9 +2,18 @@ result=0
 
 function play(){
     var CHOOSE = [Math.floor(Math.random()*3)]
-    if (CHOOSE==0){result="pedra"}
-    if (CHOOSE==1){result="papel"}
-    if (CHOOSE==2){result="tesoura"}
-    frase.innerHTML="O Computador escolheu..."
+    if (CHOOSE==0){
+        result="pedra",
+        document.getElementById( "img1" ).style.display = "none"
+    }
+    if (CHOOSE==1){
+        result="papel",
+        document.getElementById( "img1" ).style.display = "none";
+    }
+    if (CHOOSE==2){
+        result="tesoura",
+        document.getElementById( "img1" ).style.display = "inline";
+    }
+    frase.innerHTML="<br><br><br>O Computador escolheu..."
     resultado.innerHTML=result
 }
